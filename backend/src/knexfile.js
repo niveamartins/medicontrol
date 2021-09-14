2df23deb5160c75a4715529eb87d2f985f4486bf
Nivea <niveamartins@dcc.ufrj.br>
2021-09-13 22:27:45 -0300

    project backend strutured

---
+++ backend/src/knexfile.js
@@ -0,0 +1,50 @@
+module.exports = {
+    development: {
+        client: 'pg',
+        connection: {
+          host : process.env.DB_HOST,
+          user : process.env.DB_USER,
+          password : process.env.DB_PASSWORD,
+          database : process.env.DB_NAME,
+          charset: 'utf8'
+        },
+        migrations: {
+          directory: './database/migrations'
+        },
+        seeds: {
+          directory: './database/seeds'
+        }
+      },
+
+      staging: {
+        client: 'postgresql',
+        connection: {
+          database: 'my_db',
+          user:     'username',
+          password: 'password'
+        },
+        pool: {
+          min: 2,
+          max: 10
+        },
+        migrations: {
+          tableName: 'knex_migrations'
+        }
+      },
+    
+      production: {
+        client: 'postgresql',
+        connection: {
+          database: 'my_db',
+          user:     'username',
+          password: 'password'
+        },
+        pool: {
+          min: 2,
+          max: 10
+        },
+        migrations: {
+          tableName: 'knex_migrations'
+        }
+      }
+  };
