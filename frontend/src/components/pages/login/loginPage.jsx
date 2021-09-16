2837b4d6280a576cd88d797b6881553e9908c1e6
Nivea <niveamartins@dcc.ufrj.br>
2021-09-16 00:29:04 -0300

    starting login layout

--- frontend/src/components/pages/login/loginPage.jsx
+++ frontend/src/components/pages/login/loginPage.jsx
@@ -1,10 +1,31 @@
 
+import styled from 'styled-components';
+
 function LoginPage() {
     return (
         <>
+            <Box>
+                <LoginTab>
+                    Um teste
+                </LoginTab>
+                
+            </Box>
         </>
     );
   }
+
+  const Box = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: end;
+`;
+
+  const LoginTab = styled.div`
+  background-color: #3B929C;
+  width: 40%;
+  height: 100vw;
+`;
   
   export default LoginPage;
   
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LoginPage from './components/pages/login/loginPage';
 import MedicinesPage from './components/pages/medicines/medicinesPage';
 
 
@@ -7,6 +8,7 @@ export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
+                <Route path="/login" exact component={LoginPage} />
                 <Route path="/medicines" exact component={MedicinesPage} />
             </Switch>
         </BrowserRouter>
